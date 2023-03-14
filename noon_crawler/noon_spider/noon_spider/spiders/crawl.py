8a871791529b5e02875618db11be08f5c1df9602
Sagar <sagar.arora198@gmail.com>
2023-03-14 17:16:33 +0530

    deleted

--- noon_crawler/noon_spider/noon_spider/spiders/crawl.py
+++ noon_crawler/noon_spider/noon_spider/spiders/crawl.py
@@ -6,15 +6,16 @@ from scrapy import Request
 class dealsSpider(scrapy.Spider):
     name = 'deals'
 
-    url = 'https://www.noon.com/uae-en/all-p-fbn-ae/generic/?limit=150&page={}&sort%5Bby%5D=popularity&sort%5Bdir%5D=desc'
-    def start_requests(self):
-        for i in range(1,51):
-            yield Request(url=self.url.format(i))
+    url = 'https://www.noon.com/uae-en/all-p-fbn-ae/generic/?limit=150&page=3&sort%5Bby%5D=popularity&sort%5Bdir%5D=desc'
+    # def start_requests(self):
+    #     for i in range(1,51):
+    #         yield Request(url=self.url.format(i))
     def parse(self,response):
         href_links = response.css('.productContainer a::attr(href)').extract()
         for href in href_links:
             full_link = 'https://www.noon.com' + href
-            yield Request(full_link,callback=self.parse_product)
+            # yield Request(full_link,callback=self.parse_product)
+            yield {'link':full_link}
     def parse_product(self,response):
         items = NoonSpiderItem()
         product_links = response.url
